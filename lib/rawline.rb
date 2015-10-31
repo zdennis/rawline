551e43c8295d0f5a52a0d1a924c3fa4ba44a5f56
Zach Dennis <zach.dennis@gmail.com>
2015-10-31 09:59:58 -0400

    Don't use a default editor on RawLine module.
    * Move away from readline compatibility

--- lib/rawline.rb
+++ lib/rawline.rb
@@ -50,61 +50,9 @@ require "#{dir}/rawline/terminal/vt220_terminal"
 require "#{dir}/rawline/history_buffer"
 require "#{dir}/rawline/line"
 require "#{dir}/rawline/prompt"
+require "#{dir}/rawline/event_loop"
+require "#{dir}/rawline/event_registry"
 require "#{dir}/rawline/editor"
 
 module RawLine
-  self.instance_eval do
-    class << self;  attr_accessor :editor end
-    @editor = RawLine::Editor.new
-
-    @implemented_methods =
-      [
-        :completion_proc,
-        :completion_proc=,
-        :completion_matches,
-        :completion_append_character,
-        :completion_append_character=,
-        :basic_word_break_characters,
-        :basic_word_break_characters=,
-        :completer_word_break_characters,
-        :completer_word_break_characters=,
-        :library_version,
-        :clear_history,
-        :match_hidden_files,
-        :match_hidden_files=
-      ]
-
-    @implemented_methods.each do |meth|
-      self.class.module_eval do
-        define_method meth do |*args|
-          case args.length
-          when 0 then
-            @editor.send(meth)
-          when 1 then
-            @editor.send(meth, args[0])
-          else
-            raise ArgumentError, "There are no Readline methods with more than one parameter"
-          end
-        end
-      end
-    end
-
-    private
-
-    HISTORY = RawLine.editor.history
-    FILENAME_COMPLETION_PROC = RawLine.editor.filename_completion_proc
-
-    readline_method = lambda do
-
-      def readline(prompt="", add_history=false)
-        RawLine.editor.read prompt, add_history
-      end
-
-      alias rawline readline
-    end
-
-    self.class.module_eval { readline_method.call }
-    self.module_eval { readline_method.call }
-
-  end
 end
